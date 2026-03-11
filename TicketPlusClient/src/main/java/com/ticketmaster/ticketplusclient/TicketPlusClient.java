6f58768626e03827d26df1f3c0131c5cb8b80a6a
Christian <Christian@192.168.1.104>
2026-03-11 20:11:31 +0100

    Proyecto inicial creado.

---
+++ TicketPlusClient/src/main/java/com/ticketmaster/ticketplusclient/TicketPlusClient.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ticketmaster.ticketplusclient;
+
+/**
+ *
+ * @author Christian
+ */
+public class TicketPlusClient {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
