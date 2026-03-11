86b644dbfeadef5f30ee3339e14821ea03a9e760
Christian <Christian@192.168.1.104>
2026-03-11 21:03:34 +0100

    Interfaces LoginGUI y DashboardGUI creadas. Archivo POM creado con dependencias.

--- TicketPlusClient/src/main/java/com/ticketmaster/ticketplusclient/TicketPlusClient.java
+++ TicketPlusClient/src/main/java/com/ticketmaster/ticketplusclient/TicketPlusClient.java
@@ -4,13 +4,46 @@
 
 package com.ticketmaster.ticketplusclient;
 
+import com.ticketmaster.ticketplusclient.gui.LoginGUI;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+
 /**
  *
  * @author Christian
  */
-public class TicketPlusClient {
+public class TicketPlusClient extends JFrame implements CommandLineRunner {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private LoginGUI loginGui;
+
+    /**
+     * Sobre carga de clase Main con la incializacion del JFrame
+     *
+     */
+    public TicketPlusClient(){
+        loginGui = new LoginGUI();
+    }
+
+    /**
+     * Metodo static main que invoco SpringApplication.run
+     *
+     * @param args
+     */
+    public static void main(String[] args){
+        /**
+         * Se pasa la propia clase TicketPlusClient.class como argumentos
+         */
+        SpringApplication.run(TicketPlusClient.class, args);
+    }
+    /**
+     *Metodo run invocado por SpringApplication
+     *
+     * @param args
+     * @throws Exception
+     */
+    public void run(String... args) throws Exception {
+        SwingUtilities.invokeLater(() -> loginGui.setVisible(true));
     }
 }
